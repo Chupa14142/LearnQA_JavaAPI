c6d2acd5a71a1f531c8a2ceb1f0c319735616d27
Chupa14142 <chupa14142@gmail.com>
2022-11-28 15:59:35 +0200

    Ex3 : Создание репозитория

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
+
+public class HelloWorldTest {
+
+    @Test
+    public void TestEx3() {
+        System.out.println("Hello from Eugene");
+
+    }
+    
+}
